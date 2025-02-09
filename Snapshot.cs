df4382fc6349702698c931a3cc0d0d3493ee44e3
shamsWMM <shams.alsaegh@gmail.com>
2025-02-09 15:39:01 +1100

    extract text content

--- Snapshot.cs
+++ Snapshot.cs
@@ -1,6 +1,8 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Microsoft.Azure.Functions.Worker.Extensions.Sql;
+using HtmlAgilityPack;
+using System.Xml.XPath;
 
 namespace WebsiteWatcher;
 
@@ -14,6 +16,35 @@ public class Snapshot(ILogger<Snapshot> logger)
         {
             logger.LogInformation($"{change.Operation}");
             logger.LogInformation($"Id: {change.Item.Id} Url: {change.Item.Url}");
+            if (change.Operation != SqlChangeOperation.Insert)
+            {
+                continue;
+            }
+
+            if (string.IsNullOrEmpty(change.Item.XPathExpression))
+            {
+                logger.LogInformation($"No XPathExpression for {change.Item.Url}");
+                continue;
+            }
+
+            try
+            {
+                HtmlWeb web = new ();
+                HtmlDocument doc = web.Load(change.Item.Url);
+
+                var divWithContent = doc.DocumentNode.SelectSingleNode(change.Item.XPathExpression);
+                var content = divWithContent != null ? divWithContent.InnerText.Trim() : "No content";
+
+                logger.LogInformation(content);
+            }
+            catch (XPathException ex)
+            {
+                logger.LogError($"Invalid XPathExpression {change.Item.Url}: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Error {change.Item.Url}: {ex.Message}");
+            }
         }
     }
 }
