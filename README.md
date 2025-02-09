184863200b8059b85c59be677fffa7a5764b2d21
shamsWMM <shams.alsaegh@gmail.com>
2025-02-09 13:48:15 +1100

    add SQL output binding to Register

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ References: [Azure Functions for Developers](https://github.com/LinkedInLearning
 ## Required
 - .NET SDK
 - Azure Functions Core Tools
+
+## SQL Database
+- https://hub.docker.com/r/microsoft/azure-sql-edge
+- Azure Data Studio
+- "Data Source=localhost;User Id=sa;Password=Admin123$;Integrated Security=True;TrustServerCertificate=true;Trusted_Connection=false"
--- Register.cs
+++ Register.cs
@@ -1,15 +1,15 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System.IO;
 using System.Text.Json;
+using Microsoft.Azure.Functions.Worker.Extensions.Sql;
 
 namespace WebsiteWatcher;
 public class Register(ILogger<Register> logger)
 {
     [Function(nameof(Register))]
-    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+    [SqlOutput("dbo.Websites", "WebsiteWatcherConnect")]
+    public async Task<Website> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
     {
         logger.LogInformation("C# HTTP trigger function processed a request.");
         var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
@@ -17,7 +17,7 @@ public class Register(ILogger<Register> logger)
         var newWebsite = JsonSerializer.Deserialize<Website>(requestBody, options);
         newWebsite.Id = Guid.NewGuid();
 
-        return new OkObjectResult(newWebsite);
+        return newWebsite;
     }
 }
 
